b76839ec844cba14556412f7030bef4165981d26
Ravi Kalla <ravi2523096@gmail.com>
2018-08-07 16:13:52 -0400

    updated entities

--- ui/src/main/webapp/app/home/home.route.ts
+++ ui/src/main/webapp/app/home/home.route.ts
@@ -7,6 +7,6 @@ export const HOME_ROUTE: Route = {
     component: HomeComponent,
     data: {
         authorities: [],
-        pageTitle: 'Welcome to microservices application!'
+        pageTitle: 'Welcome, Java Hipster!'
     }
 };
